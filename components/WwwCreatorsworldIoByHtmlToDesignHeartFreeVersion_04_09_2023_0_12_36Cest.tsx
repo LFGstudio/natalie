d04446ec5eb58151d4a2a06a7c567d9ee76b30e7
LFGstudio <38088103+LFGstudio@users.noreply.github.com>
2023-09-22 20:09:27 +0200

    Initial commit

---
+++ components/WwwCreatorsworldIoByHtmlToDesignHeartFreeVersion_04_09_2023_0_12_36Cest.tsx
@@ -0,0 +1,16 @@
+import styles from "./WwwCreatorsworldIoByHtmlToDesignHeartFreeVersion_04_09_2023_0_12_36Cest.module.css";
+export const WwwCreatorsworldIoByHtmlToDesignHeartFreeVersion_04_09_2023_0_12_36Cest =
+  ({ override }: { override?: React.CSSProperties }) => {
+    return (
+      <div
+        className={
+          styles.wwwCreatorsworldIoByHtmlToDesignHeartFreeVersion_04_09_2023_0_12_36CestComponent
+        }
+        style={override}
+      >
+        <div className={styles.section}>
+          <p className={styles.rarephenomenonNyc}>Rarephenomenon.nyc</p>
+        </div>
+      </div>
+    );
+  };
